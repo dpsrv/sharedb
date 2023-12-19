40ac95d8ed3ec37a9560310b958629b9a59016d5
Max <max@maxf.net>
2023-12-19 17:44:05 -0500

    updated

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var WebSocket = require('ws');
-var http = require('http');
-var ShareDB = require('sharedb');
-var WebSocketJSONStream = require('@teamwork/websocket-json-stream');
+const express = require('express');
+const WebSocket = require('ws');
+const http = require('http');
+const ShareDb = require('sharedb');
+ShareDb.logger.setMethods({
+  info: console.log,
+  warn: console.log,
+  error: console.log
+});
 
-var app = express();
-var server = http.createServer(app);
-var webSocketServer = new WebSocket.Server({server: server});
+const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 
-var backend = new ShareDB();
+const redis = require('redis');
+const RedisPubsub = require('sharedb-redis-pubsub');
+
+// const RedisPubsub = require('sharedb-redis-pubsub');
+
+const app = express();
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({server: server});
+
+const backend = new ShareDb();
+const ShareDbMongo = require('sharedb-mongo');
+
 webSocketServer.on('connection', (webSocket) => {
-  var stream = new WebSocketJSONStream(webSocket)
+  const stream = new WebSocketJSONStream(webSocket)
   backend.listen(stream)
 });
 
